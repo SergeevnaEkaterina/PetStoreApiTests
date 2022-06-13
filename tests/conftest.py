db40bb8450ef071e3a6bd4dc276016cbc6231c9f
Ekaterina Gerasina <gerasinaes@mail.ru>
2022-06-13 18:03:05 +0300

    parallel run and reports both default and allure generated

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,26 +8,26 @@ from data_generation.pet_builder import generate_test_pet
 from data_generation.user_builder import generate_test_user
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="function")
 def existing_pet():
     pet = generate_test_pet()
     PetClient().get_pet_endpoints().create(pet)
     yield pet
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="function")
 def generated_pet():
     yield generate_test_pet()
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="function")
 def existing_order():
     order = generate_test_order()
     OrderClient().get_order_endpoints().create(order)
     yield order
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="function")
 def generated_order():
     yield generate_test_order()
 
